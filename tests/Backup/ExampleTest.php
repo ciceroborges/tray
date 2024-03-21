644f2655f79f0915b8c12a3a60469940eef8228f
Cícero Borges <ciceropborges@gmail.com>
2024-03-20 22:37:35 -0300

    framework tests backup...

--- tests/Backup/ExampleTest.php
+++ tests/Backup/ExampleTest.php
@@ -12,8 +12,8 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/seller');
+        $response = $this->get('/');
 
-        $response->assertStatus(302);
+        $response->assertStatus(200);
     }
 }
